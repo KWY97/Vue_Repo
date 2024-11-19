b6a2dab753ed84097278bcccda4809eff9668b86
KWY97 <kwy1547@gmail.com>
2024-11-19 12:14:15 +0900

    vue_wiht_django_2 스켈레톤 코드 수정

--- 241114_Vue_with_DRF_2/online/django-pjt/articles/models.py
+++ 241114_Vue_with_DRF_2/online/django-pjt/articles/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class Article(models.Model):
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
